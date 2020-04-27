e561911a7a901220c6d816d8cb72bcd797a1acc4
biprajeet <biprajeet.pal@gmail.com>
2020-04-27 20:56:11 +0530

    dp solution

--- src/main/java/cp/practice/leetcode30day/squareboxfinding/Solution.java
+++ src/main/java/cp/practice/leetcode30day/squareboxfinding/Solution.java
@@ -39,4 +39,35 @@ class Solution {
         
         return k*k;
     } 
+    
+    //USING DP
+    
+    public int maximalSquare2(char[][] matrix) {
+        if(matrix == null || matrix.length == 0){
+            return 0;
+        }
+        int[][] dp = new int[matrix.length][matrix[0].length];
+        markOnes(matrix, dp);
+        
+        return markOnes(matrix, dp);
+    }
+    
+    private int markOnes(char[][] matrix, int[][] dp){
+        int currentMax = 0;
+        for(int j = 0; j < matrix[0].length; j++){
+            for(int i = 0; i < matrix.length; i++){
+                if (matrix[i][j] == '0'){
+                    dp[i][j] = 0;
+                } else {
+                    int squareSize = 1;
+                    if(j != 0 && i != 0){
+                        squareSize += Math.min(dp[i - 1][j], Math.min(dp[i][j-1], dp[i-1][j-1]));
+                    }
+                    dp[i][j] = squareSize;
+                    currentMax = Math.max(squareSize, currentMax);
+                }
+            }
+        }
+        return currentMax * currentMax;
+    }
 }
